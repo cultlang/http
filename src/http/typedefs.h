b76ce9e92f7b8a4389f7681a27309184311f3e94
Clark Rinker <cgrinker@gmail.com>
2018-05-21 14:12:51 -0700

    Purging MPIR from type system

--- src/http/typedefs.h
+++ src/http/typedefs.h
@@ -6,7 +6,6 @@ typedef instance<int8_t> t_i8;
 typedef instance<uint16_t> t_i16;
 typedef instance<uint32_t> t_i32;
 typedef instance<uint64_t> t_i64;
-typedef instance<mpz_class> t_i;
 
 typedef instance<uint8_t> t_u8;
 typedef instance<uint16_t> t_u16;
@@ -16,7 +15,7 @@ typedef instance<uint64_t> t_u64;
 
 typedef instance<float> t_f32;
 typedef instance<double> t_f64;
-typedef instance<mpf_class> t_f;
+
 
 typedef Request a_req;
 typedef Response a_rep;
